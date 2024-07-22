97fc1b93b344bea2299ee5666be8e5a1eb5fc2f2
playerminecraft <duong92618@gmail.com>
2024-07-22 12:58:08 +0700

    add orgin master

--- assets/js/Library/card.js
+++ assets/js/Library/card.js
@@ -1,12 +1,13 @@
-window.onload = function () {
+function adjustCardHeights() {
   // Lấy tất cả các thẻ div có lớp 'card'
-  const cards = document.querySelectorAll(".new-item__title");
+  const cards = document.querySelectorAll(".card");
 
   // Khởi tạo biến để lưu chiều cao tối đa
   let maxHeight = 0;
 
   // Tìm chiều cao lớn nhất
   cards.forEach((card) => {
+    card.style.height = "auto"; // Reset height để tính toán chính xác
     const cardHeight = card.clientHeight; // Lấy chiều cao của thẻ
     if (cardHeight > maxHeight) {
       maxHeight = cardHeight;
@@ -17,4 +18,8 @@ window.onload = function () {
   cards.forEach((card) => {
     card.style.height = `${maxHeight}px`;
   });
-};
+}
+
+// Gọi hàm khi tải trang và khi thay đổi kích thước cửa sổ
+window.addEventListener("load", adjustCardHeights);
+window.addEventListener("resize", adjustCardHeights);
